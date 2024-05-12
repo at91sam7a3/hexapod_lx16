338b99e928473191c4679d28182a4c0dfcfe6f7b
Oleksii Vynogradov <at91sam7a3@gmail.com>
2024-05-12 18:41:27 +0200

    fixed compilation
    still linking with wiringPi is needed

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,31 @@
-int main(int argc, char*argv[])
+#include "lib/hexapod_kinematics/src/platform.hpp"
+#include "lib/lx16lib/lx16driver.h"
+
+static lx16driver driver("/dev/ttyS0", true);
+
+void setServo(int id, double angle)
 {
+    int angleToLx = (angle +45.0 ) / 0.27;
+    driver.ServoMoveTimeWrite(id,angleToLx,100);
+}
+
+void sleepMs(int sleepTime)
+{
+    ;
+}
+
+void turnOnPowerToLegs()
+{
+    wiringPiSetup ();
+    pinMode (7,OUTPUT); //Turn on power relay
+    digitalWrite (7,true);
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << (driver.isOperational() ? "Servo driver OK" : "Servo driver failed to start") << std::endl;
+    turnOnPowerToLegs();
+    hexapod::Platform platform(&sleepMs, &setServo);
+    platform.ParkLegs();
     return 0;
 }
