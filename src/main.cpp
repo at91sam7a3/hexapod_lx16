aa7e6b0cb182af0f004884b6e444f56195c53041
Oleksii Vynogradov <at91sam7a3@gmail.com>
2024-06-06 22:31:58 +0200

    build script tweak

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "../lib/hexapod_kinematics/src/platform.hpp"
-#include "../lib/lx16lib/lx16driver.h"
+#include "../../hexapod_kinematics/src/platform.hpp"
+#include "../../lx16lib/lx16driver.h"
 #include <chrono>
 #include <thread>
 #include <wiringPi.h>
@@ -8,9 +8,7 @@ static lx16driver driver("/dev/ttyS0", true);
 
 void setServo(int id, double angle)
 {
-    std::cout << "------------" << std::endl;
-    std::cout << "set motor "<<id<<" to angle "<<angle<<std::endl;
-     int angleToLx = (angle + 45.0) / 0.27;
+    int angleToLx = (angle + 45.0) / 0.27;
     driver.ServoMoveTimeWrite(id, angleToLx, 100);
 }
 
@@ -27,6 +25,33 @@ void turnOnPowerToLegs()
     digitalWrite(7, true);
 }
 
+
+void checkServos()
+{
+    std::cout<<"Checking connected servos"<<std::endl;
+    for (int i =0 ; i<18; ++i)
+    {
+        std::cout<<"Servo #"<<i<<"...";
+        int voltage=driver.ServoVoltageRead(i);
+        if(voltage == 0)
+        {
+            std::cout<<"Error"<<std::endl;
+            continue;
+        }
+        else
+        {
+            std::cout<<"OK"<<std::endl;
+        }
+        float volt = voltage / 1000;
+        std::cout<<"Voltage = "<<volt<<std::endl;
+        std::cout<<"Adjustments angle = " << driver.ServoAdjustAngleGet(i)<<std::endl;
+        std::pair<int,int> bounds = driver.GetAngleLimits(i);
+        std::cout<<"Limits "<<bounds.first<<" .. "<<bounds.second<<std::endl;
+        std::cout<<"Current angle "<<driver.ServoPositionRead(i)<<std::endl;
+        std::cout<<"Status = "<<(int)driver.GetServoErrorStatus<<std::endl;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     std::cout << (driver.isOperational() ? "Servo driver OK"
@@ -34,6 +59,7 @@ int main(int argc, char *argv[])
               << std::endl;
     turnOnPowerToLegs();
     hexapod::Platform platform(&sleepMs, &setServo);
+    checkServos();
     platform.parkLegs();
     std::cout << "To start moving, enter next data" << std::endl;
     float x, y, r = 0;
@@ -47,8 +73,8 @@ int main(int argc, char *argv[])
     std::cout << "R: ";
     std::cin >> r;
     std::cout << std::endl;
-    platform.setVelocity({x, y}, r);
-    platform.startMovementThread();
+   // platform.setVelocity({x, y}, r);
+    //platform.startMovementThread();
     std::cout << "Movement started, press enter to exit" << std::endl;
     std::cin >> x;
     return 0;
